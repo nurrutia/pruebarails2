bbcff6a151fbd93862c65fd566e543a9c482ebb9
Nicolas Urrutia <nurrutialeuno@gmail.com>
2017-07-19 20:47:58 -0400

    scope commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user, optional: true
+
+  scope :by_size, ->(size) { where("size = ?", size) }
+  scope :usernil, -> { where("user_id == nil") }
 end
