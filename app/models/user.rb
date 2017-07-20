4a74f4f3113f5d6c2e2607e735619b6f4084ecf4
Nicolas Urrutia <nurrutialeuno@gmail.com>
2017-07-19 20:20:39 -0400

    tercer commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+	has_many  :items
+	validates :name, presence: true
+	validates :email, uniqueness: true
+
+   before_save :capitalize_name
+
+   def capitalize_name
+      self.name.capitalize
+   end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719233207) do
+ActiveRecord::Schema.define(version: 20170720000739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,18 @@ ActiveRecord::Schema.define(version: 20170719233207) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "items", "categories"
+  add_foreign_key "items", "users"
 end
