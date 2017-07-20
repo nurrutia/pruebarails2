912db7516fcae798eec516abaf4ba90641ee1a76
Nicolas Urrutia <nurrutialeuno@gmail.com>
2017-07-19 21:54:08 -0400

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'paginas#inicio'
   get 'paginas/inicio'
-
   get 'paginas/eliminar'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
